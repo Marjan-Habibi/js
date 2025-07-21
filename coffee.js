60efca4c361e9d0eff4a2e30c2f4ee17d9175c8a
Marjan-Habibi <marjanhabibi875@gmail.com>
2025-07-21 17:13:52 +0430

    Update coffee.js

--- coffee.js
+++ coffee.js
@@ -1,40 +1,36 @@
 let username = prompt("Enter Your Username(admin or user)");
 let password = prompt("Enter Your Password!");
 
-if ((username === "admin" || username === "user") && password === "1234")
-{
+if ((username === "admin" || username === "user") && password === "1234") {
     alert("Login successful!");
-}
-else{
-    alert("Invalid credentials. Access denied.Try again!")
+} else {
+    alert("Invalid credentials. Access denied. Try again!");
     throw new Error("Stop execution");
 }
+
 let role = username;
 let securityLevel = username === "admin" ? "high" : "low";
 
-
 let customerName = prompt("What is your Name?");
-let age =  parseInt(prompt("How old are you?"));
-let  coffeeType = prompt("Choose coffee type: espresso, latte, or cappuccino.")
-let  quantity = parseInt(prompt("How many cups do you want?"));
+let age = parseInt(prompt("How old are you?"));
+let coffeeType = prompt("Choose coffee type: espresso, latte, or cappuccino.");
+let quantity = parseInt(prompt("How many cups do you want?"));
 
 let pricePerCup;
-if(coffeeType === "espresso"){
+if (coffeeType === "espresso") {
     pricePerCup = 2.5;
-}
-else if(coffeeType === "latte"){
+} else if (coffeeType === "latte") {
     pricePerCup = 3.5;
-}
-else if(coffeeType === "cappuccino"){
+} else if (coffeeType === "cappuccino") {
     pricePerCup = 4.0;
-}
-else{
+} else {
     alert("Invalid coffee type!");
     throw new Error("Invalid order");
 }
-let orginalTotal =  pricePerCup*quantity;
-let discount = (age < 18 || age > 60 ) ? orginalTotal * 0.10 : 0;
-let finalTotal = orginalTotal - discount;
+
+let originalTotal = pricePerCup * quantity;
+let discount = (age < 18 || age > 60) ? originalTotal * 0.10 : 0;
+let finalTotal = originalTotal - discount;
 
 alert(`Hello ${customerName}!
 You ordered ${quantity} ${coffeeType}(s).
@@ -43,7 +39,16 @@ Discount: $${discount.toFixed(2)}
 Final total: $${finalTotal.toFixed(2)}`);
 
 let split = parseInt(prompt("How many people are splitting the bill? (1, 2, or 3)"));
+if (![1, 2, 3].includes(split)) {
+    alert("Invalid number of people for splitting the bill.");
+    throw new Error("Invalid split");
+}
+
 let tipPercent = parseInt(prompt("Enter tip percentage (0, 5, 10, or 15):"));
+if (![0, 5, 10, 15].includes(tipPercent)) {
+    alert("Invalid tip percentage.");
+    throw new Error("Invalid tip");
+}
 
 let tipAmount = (finalTotal * tipPercent) / 100;
 let totalWithTip = finalTotal + tipAmount;
